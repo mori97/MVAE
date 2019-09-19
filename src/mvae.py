028107710e7c3cabbb126a4d2f084c89aa296bb1
mori97 <du@sap.ist.i.kyoto-u.ac.jp>
2019-09-19 17:25:37 +0900

    Add mvae.py

---
+++ src/mvae.py
@@ -0,0 +1,78 @@
+import numpy as np
+import torch
+
+from ilrma import ilrma
+
+EPS = 1e-9
+
+
+def mvae(mix, model, n_iter, device):
+    """Implementation of Multichannel Conditional VAE.
+    It only works in the determined case (n_sources == n_channels).
+
+    Args:
+        mix (numpy.ndarray): (n_frequencies, n_channels, n_frames)
+            STFT representation of the observed signal.
+        model (cvae.CVAE): Trained Conditional VAE model.
+        n_iter (int): Number of iterations.
+        device (torch.device): Device used for computation.
+
+    Returns:
+        tuple[numpy.ndarray, numpy.ndarray]: Tuple of separated signal and
+            separation matrix. The shapes of separated signal and separation
+            matrix are (n_frequencies, n_sources, n_frames) and
+            (n_frequencies, n_sources, n_channels), respectively.
+    """
+    n_freq, n_src, n_frame = mix.shape
+
+    sep, sep_mat = ilrma(mix, n_iter=30, n_basis=2)
+    sep_pow = np.power(np.abs(sep), 2)  # (n_freq, n_src, n_frame)
+    c = torch.full((n_src, model.n_speakers), 1 / model.n_speakers,
+                   device=device, requires_grad=True)
+    log_g = torch.full((n_src, 1, 1), model.log_g.item(), device=device)
+
+    with torch.no_grad():
+        sep_pow_tensor = torch.from_numpy(sep_pow).transpose(0, 1).to(device)
+        z, _ = model.encode(sep_pow_tensor, c)
+        sigma_sq = (model.decode(z, c) + log_g).exp()
+        sigma_reci = (1 / sigma_sq).cpu().numpy()
+    z.requires_grad = True
+
+    eye = np.tile(np.eye(n_src), (n_freq, 1, 1))
+
+    for _ in range(n_iter):
+        for src in range(n_src):
+            h = sigma_reci[src, :, :, None] @ np.ones((1, n_src))
+            h = mix.conj() @ (mix.swapaxes(1, 2) * h)
+            u_mat = h.swapaxes(1, 2) / n_frame
+            h = sep_mat @ u_mat + EPS * eye
+            sep_mat[:, src, :] = np.linalg.solve(h, eye[:, :, src]).conj()
+            h = sep_mat[:, src, None, :] @ u_mat
+            h = (h @ sep_mat[:, src, :, None].conj()).squeeze(2)
+            sep_mat[:, src, :] = (sep_mat[:, src, :] / np.sqrt(h).conj())
+
+        np.matmul(sep_mat, mix, out=sep)
+        np.power(np.abs(sep), 2, out=sep_pow)
+
+        optimizer = torch.optim.Adam((z, c), lr=1e-4)
+        sep_pow_tensor = torch.from_numpy(sep_pow).to(device).transpose(0, 1)
+        for _ in range(100):
+            log_sigma_sq = model.decode(z, torch.softmax(c, dim=1)) + log_g
+            loss = torch.sum(
+                log_sigma_sq + sep_pow_tensor / log_sigma_sq.exp())
+            model.zero_grad()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        with torch.no_grad():
+            sigma_sq = (model.decode(z, torch.softmax(c, dim=1)) + log_g).exp()
+            lbd = torch.sum(sep_pow_tensor / sigma_sq, dim=(1, 2))
+            lbd = lbd / n_freq / n_frame / log_g.squeeze(2).squeeze(1).exp()
+            log_g[:, 0, 0] += torch.log(lbd)
+            sigma_sq *= lbd.unsqueeze(1).unsqueeze(2)
+            sep_mat *= lbd.unsqueeze(0).unsqueeze(2).cpu().numpy()
+
+            sigma_reci = (1 / sigma_sq).cpu().numpy()
+
+    return sep, sep_mat
