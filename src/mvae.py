ce9424f702173f936053cb87073bee452b9cecec
mori97 <du@sap.ist.i.kyoto-u.ac.jp>
2019-10-07 21:11:53 +0900

    Add GPU support to MVAE

--- src/mvae.py
+++ src/mvae.py
@@ -1,7 +1,11 @@
+try:
+    import cupy as cp
+except ImportError:
+    cp = None
 import numpy as np
 import torch
 
-from common import projection_back
+from common import projection_back, to_cupy, to_tensor
 from ilrma import ilrma
 
 EPS = 1e-9
@@ -12,7 +16,7 @@ def mvae(mix, model, n_iter, device, proj_back=True, return_sigma=False):
     It only works in the determined case (n_sources == n_channels).
 
     Args:
-        mix (numpy.ndarray): (n_frequencies, n_channels, n_frames)
+        mix (ndarray): (n_frequencies, n_channels, n_frames)
             STFT representation of the observed signal.
         model (cvae.CVAE): Trained Conditional VAE model.
         n_iter (int): Number of iterations.
@@ -22,47 +26,70 @@ def mvae(mix, model, n_iter, device, proj_back=True, return_sigma=False):
             each speaker.
 
     Returns:
-        tuple[numpy.ndarray, numpy.ndarray]: Tuple of separated signal and
+        tuple[ndarray, ndarray]: Tuple of separated signal and
             separation matrix. The shapes of separated signal and separation
             matrix are (n_frequencies, n_sources, n_frames) and
             (n_frequencies, n_sources, n_channels), respectively.
     """
+    if isinstance(mix, np.ndarray):
+        if_use_cuda = False
+        xp = np
+    elif isinstance(mix, cp.ndarray):
+        if_use_cuda = True
+        xp = cp
+    else:
+        raise ValueError('A numpy.ndarray or cupy.ndarray instance should be '
+                         'given as `mix` argument')
+
     n_freq, n_src, n_frame = mix.shape
 
     sep, sep_mat = ilrma(mix, n_iter=30, n_basis=2)
-    sep_pow = np.power(np.abs(sep), 2)  # (n_freq, n_src, n_frame)
+    sep_pow = xp.power(xp.abs(sep), 2)  # (n_freq, n_src, n_frame)
     c = torch.full((n_src, model.n_speakers), 1 / model.n_speakers,
                    device=device, requires_grad=True)
     log_g = torch.full((n_src, 1, 1), model.log_g.item(), device=device)
 
     with torch.no_grad():
-        sep_pow_tensor = torch.from_numpy(sep_pow).transpose(0, 1).to(device)
+        if if_use_cuda:
+            sep_pow_tensor = to_tensor(sep_pow).transpose(0, 1)
+        else:
+            sep_pow_tensor =\
+                torch.from_numpy(sep_pow).transpose(0, 1).to(device)
         sep_pow_tensor.clamp_(EPS)
         z, _ = model.encode(sep_pow_tensor, c)
         sigma_sq = (model.decode(z, c) + log_g).exp()
         sigma_sq.clamp_(min=EPS)
-        sigma_reci = (1 / sigma_sq).cpu().numpy()
+        sigma_reci = 1 / sigma_sq
+        if if_use_cuda:
+            sigma_reci = to_cupy(sigma_reci)
+        else:
+            sigma_reci = sigma_reci.numpy()
+
     z.requires_grad = True
 
-    eye = np.tile(np.eye(n_src), (n_freq, 1, 1))
+    eye = xp.tile(xp.eye(n_src), (n_freq, 1, 1))
 
     for _ in range(n_iter):
         for src in range(n_src):
-            h = sigma_reci[src, :, :, None] @ np.ones((1, n_src))
+            h = sigma_reci[src, :, :, None] @ xp.ones((1, n_src))
             h = mix.conj() @ (mix.swapaxes(1, 2) * h)
             u_mat = h.swapaxes(1, 2) / n_frame
             h = sep_mat @ u_mat + EPS * eye
-            sep_mat[:, src, :] = np.linalg.solve(h, eye[:, :, src]).conj()
+            sep_mat[:, src, :] = xp.linalg.solve(h, eye[:, :, src]).conj()
             h = sep_mat[:, src, None, :] @ u_mat
             h = (h @ sep_mat[:, src, :, None].conj()).squeeze(2)
-            sep_mat[:, src, :] = (sep_mat[:, src, :] / np.sqrt(h).conj())
+            sep_mat[:, src, :] = (sep_mat[:, src, :] / xp.sqrt(h).conj())
 
-        np.matmul(sep_mat, mix, out=sep)
-        np.power(np.abs(sep), 2, out=sep_pow)
-        np.clip(sep_pow, a_min=EPS, a_max=None, out=sep_pow)
+        sep = sep_mat @ mix
+        xp.power(xp.abs(sep), 2, out=sep_pow)
+        xp.clip(sep_pow, a_min=EPS, a_max=None, out=sep_pow)
 
         optimizer = torch.optim.Adam((z, c), lr=1e-3)
-        sep_pow_tensor = torch.from_numpy(sep_pow).to(device).transpose(0, 1)
+        if if_use_cuda:
+            sep_pow_tensor = to_tensor(sep_pow).transpose(0, 1)
+        else:
+            sep_pow_tensor = \
+                torch.from_numpy(sep_pow).transpose(0, 1).to(device)
         for _ in range(50):
             log_sigma_sq = model.decode(z, torch.softmax(c, dim=1)) + log_g
             loss = torch.sum(
@@ -78,14 +105,17 @@ def mvae(mix, model, n_iter, device, proj_back=True, return_sigma=False):
             lbd = lbd / n_freq / n_frame / log_g.squeeze(2).squeeze(1).exp()
             log_g[:, 0, 0] += torch.log(lbd)
             sigma_sq *= lbd.unsqueeze(1).unsqueeze(2)
-            sep_mat *= lbd.unsqueeze(0).unsqueeze(2).cpu().numpy()
-
-            sigma_reci = (1 / sigma_sq).cpu().numpy()
+            if if_use_cuda:
+                sep_mat *= to_cupy(lbd.unsqueeze(0).unsqueeze(2))
+                sigma_reci = to_cupy(1 / sigma_sq)
+            else:
+                sep_mat *= lbd.unsqueeze(0).unsqueeze(2).numpy()
+                sigma_reci = (1 / sigma_sq).numpy()
 
     # Back-projection technique
     if proj_back:
         z = projection_back(sep, mix[:, 0, :])
-        sep *= np.conj(z[:, :, None])
+        sep *= xp.conj(z[:, :, None])
 
     if return_sigma:
         return sep, sep_mat, sigma_sq.cpu().numpy()
