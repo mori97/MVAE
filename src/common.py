e3958c007e06a38525bd41eee8323fc7d223eeee
mori97 <du@sap.ist.i.kyoto-u.ac.jp>
2019-10-07 15:46:02 +0900

    Add GPU support to `projection_back`

--- src/common.py
+++ src/common.py
@@ -1,4 +1,8 @@
 import numpy as np
+try:
+    import cupy as cp
+except ImportError:
+    cp = None
 
 EPS = 1e-9
 
@@ -7,17 +11,25 @@ def projection_back(sep, ref):
     """Back-projection technique.
 
     Args:
-        sep (numpy.ndarray): (n_frequencies, n_channels, n_frames)
+        sep (ndarray): (n_frequencies, n_channels, n_frames)
             The STFT data to project back on the reference signal.
-        ref (numpy.ndarray): (n_frequencies, n_frames)
+        ref (ndarray): (n_frequencies, n_frames)
             The reference signal.
 
     Returns:
-        numpy.ndarray: (n_frequencies, n_channels)
+        ndarray: (n_frequencies, n_channels)
             The frequency-domain filter which minimizes the square error to
             the reference signal.
     """
-    num = np.sum(np.conj(ref[:, None, :]) * sep, axis=2)
-    denom = np.sum(np.abs(sep) ** 2, axis=2)
-    np.clip(denom, a_min=EPS, a_max=None, out=denom)
+    if isinstance(sep, np.ndarray):
+        xp = np
+    elif isinstance(sep, cp.ndarray):
+        xp = cp
+    else:
+        raise ValueError(
+            'A numpy.ndarray or cupy.ndarray instance should be given')
+
+    num = xp.sum(xp.conj(ref[:, None, :]) * sep, axis=2)
+    denom = xp.sum(xp.abs(sep) ** 2, axis=2)
+    xp.clip(denom, a_min=EPS, a_max=None, out=denom)
     return num / denom
